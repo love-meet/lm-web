94d812e46c03972b30ce2db3eb627c361fc091b1
Highscoretech <highscoretechng@gmail.com>
2025-09-08 00:39:59 +0100

    lo

--- src/pages/dashboard/settings/Index.jsx
+++ src/pages/dashboard/settings/Index.jsx
@@ -25,7 +25,7 @@ import {
   FaTimes
 } from 'react-icons/fa';
 import { useAuth } from '../../../context/AuthContext';
-import addNotification from 'react-push-notification';
+// import addNotification from 'react-push-notification';
 
 export default function Settings({ onClose }) {
   const { user, handleLogOut, preferences, updatePreferences } = useAuth();
@@ -55,15 +55,6 @@ export default function Settings({ onClose }) {
   });
   
 
-    const buttonClick = () => {
-      addNotification({
-          title: 'Warning',
-          subtitle: 'This is a subtitle',
-          message: 'This is a very long message',
-          theme: 'darkblue',
-          native: true // when using native, your OS will handle theming.
-      });
-  };
   const settingsSections = [
     {
       title: "Profile",
