94e791589e08618152dea9cdadfd209cbf19f9f7
Highscoretech <highscoretechng@gmail.com>
2025-09-08 08:04:50 +0100

    ko

--- src/api/axios.js
+++ src/api/axios.js
@@ -22,7 +22,6 @@ const api = axios.create({
 
 api.interceptors.request.use(
   (config) => {
-    // Do Add auth token
     const token = getCookie("token");
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
